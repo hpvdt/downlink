d43a86927a2c047c0e1db24741ad0aa2715434a7
Savo Bajic <bajic.savo@gmail.com>
2024-01-21 19:23:00 -0500

    Basic serial code tests

--- firmware/downlink/src/main.cpp
+++ firmware/downlink/src/main.cpp
@@ -1,9 +1,16 @@
 #include <Arduino.h>
 
+#ifdef STM32
+HardwareSerial RPi(PA10, PA9);
+#define Serial RPi
+#endif
+
 const int ledPins[] = {PB6, PB7, PB8};
 
 void setup() {
   for (int i = 0; i < 3; i++) pinMode(ledPins[i], OUTPUT);
+  Serial.begin(115200);
+  for (int i = 0; i < 3; i++) digitalWrite(ledPins[i], HIGH);
 }
 
 void loop() {
@@ -11,4 +18,5 @@ void loop() {
   delay(1000);
   for (int i = 0; i < 3; i++) digitalWrite(ledPins[i], LOW);
   delay(1000);
+  Serial.println(millis());
 }
